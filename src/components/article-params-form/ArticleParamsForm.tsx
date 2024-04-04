b7d45d751e8f259a622a9c48506172b94f7e7851
Petr Saparov <saparov.petr@yandex.ru>
2024-04-04 19:54:11 +0300

    2

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,61 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
+import clsx from 'clsx';
+import { SyntheticEvent, useRef, useState } from 'react';
+import { ArticleStateType, fontColors } from '../../constants/articleProps';
+import { Select } from '../select';
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	currientArticleState: ArticleStateType;
+	setCurrientArticleState: (params: any) => void;
+};
+
+export const ArticleParamsForm = ({
+	currientArticleState,
+	setCurrientArticleState,
+}: ArticleParamsFormProps) => {
+	const rootRef = useRef<HTMLElement>(null);
+	const [isOpenForm, setIsOpenForm] = useState<boolean>(false);
+	const [newFontColor, setNewFontColor] = useState(
+		currientArticleState.fontColor
+	);
+
+	const formOpenHandler = () => {
+		setIsOpenForm(!isOpenForm);
+	};
+
+	useOutsideClickClose({
+		isOpen: isOpenForm,
+		rootRef,
+		onClose: formOpenHandler,
+		onChange: setIsOpenForm,
+	});
+
+	const formSubmitHandler = (e: SyntheticEvent) => {
+		e.preventDefault();
+		setCurrientArticleState({
+			...currientArticleState,
+			fontColor: newFontColor,
+		});
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={formOpenHandler} isOpen={isOpenForm} />
+			<aside
+				ref={rootRef}
+				className={clsx(styles.container, isOpenForm && styles.container_open)}>
+				<form onSubmit={formSubmitHandler} className={styles.form}>
+					<Select
+						options={fontColors}
+						selected={newFontColor}
+						title='цвет'
+						onChange={setNewFontColor}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
